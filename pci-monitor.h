84c52d87f57b5c5fe670d3f222cc833e4f83a3b6
yuchenliu <yuchenliu@synology.com>
2020-08-12 16:15:17 +0800

    Add Add macro for DET state

---
+++ pci-monitor.h
@@ -0,0 +1,10 @@
+#define DET_NDNP 0x0
+#define DET_PDNP 0x1
+#define DET_PDEP 0x3
+#define DET_OFFLINE 0x4
+
+#define SIG_HOTPLUG 44
+
+#define STATE_PLUGIN 1
+#define STATE_UNPLUG 0
+#define STATE_UNRECOGNIZE 2
